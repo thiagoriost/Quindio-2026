9484c2d320c979ab7f191de6636e11543f11f748
Carlos A Lobo <carlos.lobo@igac.gov.co>
2026-02-11 19:29:19 -0500

    Proyecto SIG-QUINDIO 2026 (Migración) Propósito 1.Avance Definición States controles componente FilterAmb 2.Avance Implementación controles Fecha Inicio, Fecha Fin empleando componente DatePicker

--- widgets/ConsAmbiental/src/runtime/components/FilterAmb.tsx
+++ widgets/ConsAmbiental/src/runtime/components/FilterAmb.tsx
@@ -3,10 +3,14 @@
  * Sección importación componente FilterAmb
  * @date 2026-02-10
  * @author IGAC - DIP
+ * @dateUpdated 2026-02-11
+ * @changes Importar componente DatePicker
  **/
 
 import React, { useEffect } from 'react';
 import { Button, Input, Label, Radio, Select, TextInput, FloatingPanel } from 'jimu-ui';
+//Componente DatePicker
+import { DatePicker } from 'jimu-ui/basic/date-picker';
 
 import { appActions } from 'jimu-core';
 
@@ -17,16 +21,74 @@ import { appActions } from 'jimu-core';
  * @param visible
  * @param setVisibleState
  * @dateUpdated 2026-02-11
- * @changes inclusión param {visible}
- * @changes inclusión param {setVisibleState}
+ * @changes inclusión param visible
+ * @changes inclusión param setVisibleState
+ * @changes inclusión param areaLst
+ * @changes inclusión param setAreaLstState
+ * @changes inclusión param selAreaVal
+ * @changes inclusión param setSelAreaValState
+ * @changes inclusión param selAreaDis
+ * @changes inclusión param setAreaDisState
+ * @changes inclusión param tematicaLst
+ * @changes inclusión param setTematicaLstState
+ * @changes inclusión param selTematicaVal
+ * @changes inclusión param setSelTematicaValState
+ * @changes inclusión param selTematicaDis
+ * @changes inclusión param setSelTematicaDisState
+ * @changes inclusión param categorLst
+ * @changes inclusión param setCategorLstState
+ * @changes inclusión param selCategorVal
+ * @changes inclusión param setCategorValState
+ * @changes inclusión param selCategorDis
+ * @changes inclusión param setSelCategorDisState
+ * @changes inclusión param subCategorLst
+ * @changes inclusión param setSubCategorLstState
+ * @changes inclusión param selSubCategorVal
+ * @changes inclusión param setSelSubCategorValState
+ * @changes inclusión param selSubCategorDis
+ * @changes inclusión param setSelSubCategorDisState
+ * @changes inclusión param nomAmbLst
+ * @changes inclusión param setNomAmbLstState
+ * @changes inclusión param selNomAmbVal
+ * @changes inclusión param setSelNomAmbValState
+ * @changes inclusión param selNomAmbDis
+ * @changes inclusión param setSelNomAmbDisState
+ * @changes inclusión param anioAmbLst
+ * @changes inclusión param setAnioAmbLstState
+ * @changes inclusión param selAnioAmbVal
+ * @changes inclusión param setSelAnioAmbValState
+ * @changes inclusión param selAnioAmbDis
+ * @changes inclusión param setSelAnioAmbDisState
+ * @changes inclusión param mpioAmbLst
+ * @changes inclusión param setMpioAmbLstState
+ * @changes inclusión param selMpioAmbVal
+ * @changes inclusión param setSelMpioAmbValState
+ * @changes inclusión param selMpioAmbDis
+ * @changes inclusión param setSelMpioAmbDisState
+ * @changes inclusión param txtFecIniVal
+ * @changes inclusión param setTxtFecIniValState
+ * @changes inclusión param txtFecIniDis
+ * @changes inclusión param setTxtFecIniDisState
+ * @changes inclusión param txtFecFinVal
+ * @changes inclusión param setTxtFecFinValState
+ * @changes inclusión param txtFecFinDis
+ * @changes inclusión param setTxtFecFinDisState
+ * @changes inclusión param btnLimpiaDis
+ * @changes inclusión param setBtnLimpiaDisState
+ * @changes inclusión param btnSrchDis
+ * @changes inclusión param setBtnSrchDisState
  * @remarks Listado de propiedades enviadas desde el componente maestro
  * @remarks Pruebas con componente FloatingPanel desde jimu-ui
  * @remarks Fuente consulta Claude AI => https://claude.ai/chat/8298f344-84ec-44b9-b0bc-cb8328f56e40
  */
 
-const FilterAmb = function ({visible, setVisibleState}){
+const FilterAmb = function ({visible, setVisibleState, areaLst, setAreaLstState, selAreaVal, setSelAreaValState, selAreaDis, setAreaDisState, tematicaLst, setTematicaLstState, selTematicaVal, setSelTematicaValState, selTematicaDis, setSelTematicaDisState, categorLst, setCategorLstState, selCategorVal, setCategorValState, selCategorDis, setSelCategorDisState, subCategorLst, setSubCategorLstState, selSubCategorVal, setSelSubCategorValState, selSubCategorDis, setSelSubCategorDisState, nomAmbLst, setNomAmbLstState, selNomAmbVal, setSelNomAmbValState, selNomAmbDis, setSelNomAmbDisState, anioAmbLst, setAnioAmbLstState, selAnioAmbVal, setSelAnioAmbValState, selAnioAmbDis, setSelAnioAmbDisState, mpioAmbLst, setMpioAmbLstState, selMpioAmbVal, setSelMpioAmbValState, selMpioAmbDis, setSelMpioAmbDisState, txtFecIniVal, setTxtFecIniValState, txtFecIniDis, setTxtFecIniDisState, txtFecFinVal, setTxtFecFinValState, txtFecFinDis, setTxtFecFinDisState, btnLimpiaDis, setBtnLimpiaDisState, btnSrchDis, setBtnSrchDisState}){
+
+    //Sección métodos
+
+    //Sección renderizado
     return (
-        <FloatingPanel         
+        {selNomAmbDis} && <FloatingPanel         
         onHeaderClose={() => setVisibleState(false)}
         showHeaderCollapse={true}
         showHeaderClose={true}
@@ -35,7 +97,7 @@ const FilterAmb = function ({visible, setVisibleState}){
         defaultSize={{width: 332, height: 591 }}
         defaultPosition={{x: 150, y: 50}}
        >
-        <form>
+         <form>
             <div className="mb-1">
                 <Label size="default">&Aacute;rea</Label>
                 <Select 
@@ -46,57 +108,96 @@ const FilterAmb = function ({visible, setVisibleState}){
                 <Label size="default">Tem&aacute;tica</Label>
                 <Select 
                     placeholder="Seleccione"
+                    disabled={selTematicaDis}
+                    value={selTematicaVal}
                 ></Select>
             </div>
             <div className="mb-1">
                 <Label size="default">Categor&iacute;a</Label>
                 <Select 
                     placeholder="Seleccione"
+                    disabled={selCategorDis}
+                    value={selCategorVal}
                 ></Select>
             </div>
             <div className="mb-1">
                 <Label size="default">SubCategor&iacute;a</Label>
                 <Select 
                     placeholder="Seleccione"
+                    disabled={selSubCategorDis}
+                    value={selSubCategorVal}
                 ></Select>
             </div>
             <div className="mb-1">
                 <Label size="default">Nombre</Label>
                 <Select 
                     placeholder="Seleccione"
+                    disabled={selNomAmbDis}
+                    value={selNomAmbVal}
                 ></Select>
             </div>
             <div className="mb-1">
                 <Label size="default">A&ntilde;o</Label>
                 <Select 
                     placeholder="Seleccione"
+                    disabled={selAnioAmbDis}
+                    value={selAnioAmbVal}
                 ></Select>
             </div>
             <div className="mb-1">
                 <Label size="default">Municipio</Label>
                 <Select 
                     placeholder="Seleccione"
+                    disabled={selMpioAmbDis}
+                    value={selMpioAmbVal}
                 ></Select>
             </div>
             <div className="mb-1">
                 <Label size="default">Fecha inicio</Label>
-                <Input placeholder='Especifique fecha'></Input>
+                {/*<Input 
+                    placeholder='Especifique fecha'
+                    disabled={txtFecIniDis}
+                    value={txtFecIniVal}
+                >
+                </Input>*/
+                }
+                <DatePicker
+                    selectedDate={txtFecIniVal}
+                    onChange={(fecIn) => setTxtFecIniDisState (fecIn)}
+                    format="yyyy-MM-dd"
+                    showQuickNavToToday
+                ></DatePicker>
             </div>
             <div className="mb-1">
                 <Label size="default">Fecha fin</Label>
-                <Input placeholder='Especifique fecha'></Input>
+                {
+                /*<Input 
+                    placeholder='Especifique fecha'
+                    disabled={txtFecFinDis}
+                    value={txtFecFinVal}
+                >
+                </Input>*/
+                }
+                <DatePicker
+                    selectedDate={txtFecFinVal}
+                    onChange={(fecFin) => setTxtFecFinValState (fecFin)}
+                    format="yyyyMMdd"
+                ></DatePicker>
+                
             </div>
             {/* Sección botonera*/}
             <div className="btns">
                 <Button                           
                     size="default"
                     type="default"
+                    disabled={btnLimpiaDis}
                 >Limpiar
                 </Button>
                 <Button
                     htmlType="submit"              
                     size="default"
                     type="default"
+                    disabled={btnSrchDis}
                 >Buscar</Button>
             </div>
         </form>
--- widgets/ConsAmbiental/src/runtime/widget.tsx
+++ widgets/ConsAmbiental/src/runtime/widget.tsx
@@ -20,13 +20,78 @@ import FilterAmb from './components/FilterAmb';
  * @dateUpdated 2026-02-11
  * @changes Inclusion propiedad state visible
  * @changes Inclusion propiedad state setVisible
+ * @changes Inclusion propiedad state areaLst
+ * @changes Inclusion propiedad state selAreaVal
+ * @changes Inclusion propiedad state tematicaLst
+ * @changes Inclusion propiedad state selTematicaVal
+ * @changes Inclusion propiedad state selTematicaDis
+ * @changes Inclusion propiedad state categorLst
+ * @changes Inclusion propiedad state selCategorVal
+ * @changes Inclusion propiedad state selCategorDis
+ * @changes Inclusion propiedad state subCategorLst
+ * @changes Inclusion propiedad state 
  * @remarks Sección procesamiento
  */
 
 const Widget = (props: AllWidgetProps<IMConfig>) => {
   //Sección definición States
   //2026-02-11 - State Widget
-  const [visible, setVisible]               = useState (false);
+  const [visible, setVisible]               = useState <boolean>(false);
+  //State Lista campo Área
+  const [areaLst, setAreaLst]               = useState ([]);
+  //State Control Área
+  const [selAreaVal, setSelAreaVal]         = useState <number>();
+  //State asociado a Estado control Área
+  const [selAreaDis, setSelAreaDis]         = useState <boolean>(true);
+  //State Lista campo temática
+  const [tematicaLst, setTematicaLst]       = useState ([]);
+  //State Control Temática
+  const [selTematicaVal, setSelTematicaVal] = useState <number>();
+  //State asociado a Estado control Temática
+  const [selTematicaDis, setSelTematicaDis] = useState <boolean>(true);
+  //State Lista campo Categoría
+  const [categorLst, setCategorLst]         = useState ([]);
+  //State Control Categoría
+  const [selCategorVal, setSelCategorVal]   = useState <number>();
+  //State asociado a Estado control Categoría
+  const [selCategorDis, setSelCategorDis]   = useState <boolean>(true);
+  //State Lista campo Subcategoría
+  const [subCategorLst, setSubCategorLst]   = useState ([]);
+  //State Control Subcategoría
+  const [selSubCategorVal, setSelSubCategorVal]= useState <number>();
+  //State asociado a Estado control Subcategoría
+  const [selSubCategorDis, setSelSubCategorDis]= useState <boolean>(true);
+  //State Lista campo Nombre
+  const [nombAmbLst, setNomAmbLst]          = useState ([]);
+  //State Control Nombre
+  const [selNombAmbVal, setSelNombAmbVal]   = useState <string>();
+  //State asociado a Estado control Nombre
+  const [selNombAmbDis, setSelNombAmbDis]   = useState <boolean>(true);
+  //State Lista campo Año
+  const [anioAmbLst, setAnioAmbLst]         = useState ([]);
+  //State Control Año
+  const [selAnioAmbVal, setSelAnioAmbVal]   = useState <number>();
+  //State asociado a Estado control Año
+  const [selAnioAmbDis, setSelAnioAmbDis]   = useState <boolean>(true);
+  //State Lista campo Municipio
+  const [mpioLst, setMpioLst]               = useState ([]);
+  //State Control Municipio
+  const [selMpioVal, setSelMpioVal]         = useState <string>();
+  //State asociado a Estado control Municipio
+  const [selMpioDis, setSelMpioDis]         = useState <boolean>(true);
+  //State Fecha inicio
+  const [txtFecIniVal, setTxtFecIniVal]     = useState <Date>();
+  //State asociado a Estado campo Fecha inicio
+  const [txtFecIniDis, setTxtFecIniDis]     = useState <boolean>(true);
+  //State Fecha fin
+  const [txtFecFinVal, setTxtFecFinVal]     = useState <string>();
+  //State asociado a Estado campo Fecha fin
+  const [txtFecFinDis, setTxtFecFinDis]     = useState <boolean>(true);
+  //Opciones de la botonera
+  //State Opc Limpiar
+  const [btnLimpiaDis, setBtnLimpiaDis]     = useState <boolean>(false);
+  //State Opc Buscar
+  const [btnSrchDis, setBtnSrchDis]         = useState <boolean>(false);
   //Mapa
   const [jimuMapView, setJimuMapView]       = useState<JimuMapView>(); 
     
@@ -39,8 +104,62 @@ const Widget = (props: AllWidgetProps<IMConfig>) => {
   return (
     <div className="widget-demo jimu-widget m-2">
       <FilterAmb
-        visible = {visible}
+        visible         = {visible}
         setVisibleState = {setVisible}
+        areaLst         = {areaLst}
+        setAreaLstState = {setAreaLst}
+        selAreaVal      = {selAreaVal}
+        setSelAreaValState={setSelAreaVal}
+        selAreaDis      = {selAreaDis}
+        setAreaDisState = {setSelAreaDis}
+        tematicaLst     = {tematicaLst}
+        setTematicaLstState={setTematicaLst}
+        selTematicaVal  = {selTematicaVal}
+        setSelTematicaValState={setSelTematicaVal}
+        selTematicaDis  = {selTematicaDis}
+        setSelTematicaDisState = {setSelTematicaDis}
+        categorLst      = {categorLst}
+        setCategorLstState={setCategorLst}
+        selCategorVal   = {selCategorVal}
+        setCategorValState={setSelCategorVal}
+        selCategorDis   = {selCategorDis}
+        setSelCategorDisState = {setSelCategorDis}
+        subCategorLst   = {subCategorLst}
+        setSubCategorLstState={setSubCategorLst}
+        selSubCategorVal= {selSubCategorVal}
+        setSelSubCategorValState={setSelSubCategorVal}
+        selSubCategorDis= {selSubCategorDis}
+        setSelSubCategorDisState= {setSelSubCategorDis}
+        nomAmbLst       = {nombAmbLst}
+        setNomAmbLstState={setNomAmbLst}
+        selNomAmbVal    = {selNombAmbVal}
+        setSelNomAmbValState= {setSelNombAmbVal}
+        selNomAmbDis    = {selNombAmbDis}
+        setSelNomAmbDisState= {setSelNombAmbDis}
+        anioAmbLst      = {anioAmbLst}
+        setAnioAmbLstState  = {setAnioAmbLst}
+        selAnioAmbVal   = {selAnioAmbVal}
+        setSelAnioAmbValState={setSelAnioAmbVal}
+        selAnioAmbDis   = {selAnioAmbDis}
+        setSelAnioAmbDisState={setSelAnioAmbDis}
+        mpioAmbLst      = {mpioLst}
+        setMpioAmbLstState={setMpioLst}
+        selMpioAmbVal   = {selMpioVal}
+        setSelMpioAmbValState={setSelMpioVal}
+        selMpioAmbDis   = {selMpioDis}
+        setSelMpioAmbDisState={setSelMpioDis}
+        txtFecIniVal    = {txtFecIniVal}
+        setTxtFecIniValState= {setTxtFecIniVal}
+        txtFecIniDis    = {txtFecIniDis}
+        setTxtFecIniDisState= {setTxtFecIniDis}
+        txtFecFinVal    = {txtFecFinVal}
+        setTxtFecFinValState={setTxtFecFinVal}
+        txtFecFinDis    = {txtFecFinDis}
+        setTxtFecFinDisState= {setTxtFecFinDis}
+        btnLimpiaDis    = {btnLimpiaDis}
+        setBtnLimpiaDisState={setBtnLimpiaDis}
+        btnSrchDis      = {btnSrchDis}
+        setBtnSrchDisState={setBtnSrchDis}
       ></FilterAmb>
     </div>
   )
